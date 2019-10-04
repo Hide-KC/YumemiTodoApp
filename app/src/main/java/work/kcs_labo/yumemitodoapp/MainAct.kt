d05292c08bc18995dc9c834660c1a641834d8b84
Hide-KC <kcs.dev.labo@gmail.com>
2019-10-04 22:25:56 +0900

    init

---
+++ app/src/main/java/work/kcs_labo/yumemitodoapp/MainAct.kt
@@ -0,0 +1,12 @@
+package work.kcs_labo.yumemitodoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAct : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act)
+  }
+}
